6fb3052318e791299f85eb6ff309e786e459afb6
Seyong Kim <seyong@yonsei.ac.kr>
2022-04-05 22:06:48 +0900

    subtle changes

---
+++ metric.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def accuracy():
+    pass
+
+def recall():
+    pass
+
+def precision():
+    pass
+
+def f1score():
+    pass
+
---
+++ optimizer.py
@@ -0,0 +1,8 @@
+def batch():
+    pass
+
+def gradient_descent():
+    pass
+
+def stochastic_gradient_descent():
+    pass
